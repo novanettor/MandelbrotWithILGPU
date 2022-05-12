62abf7b5480b5cb4f8dc26f01f6df9213a4844f5
Tor Kristen Haugen <tor.kristen.haugen@novanet.no>
2022-05-12 13:02:51 +0200

    Iterator and image mapper interfaces

---
+++ src/Mandelbrot/IImageMapper.cs
@@ -0,0 +1,13 @@
+﻿namespace Mandelbrot
+{
+    public interface IImageMapper
+    {
+        /// <summary>
+        /// Generates a pixel bitmap from an iterations map by indexing each iterations value into a palette.
+        /// </summary>
+        /// <param name="iterationsMap">The iterations map, a 2D array of <see cref="int"/>s</param>
+        /// <param name="palette">The palette to apply, representing an array of Bgr24 color values</param>
+        /// <param name="output">The resulting bitmap</param>
+        void Map(int[,] iterationsMap, int[] palette, byte[] output);
+    }
+}
---
+++ src/Mandelbrot/IIterator.cs
@@ -0,0 +1,17 @@
+﻿namespace Mandelbrot
+{
+    public interface IIterator
+    {
+        /// <summary>
+        /// Creates an iterations map by performing the Mandelbrot iteration Z -> Z^2 + C for all
+        /// complex values in the given range in the complex plane.
+        /// </summary>
+        /// <param name="start">The complex number at which to start (the upper left corner in the complex plane)</param>
+        /// <param name="width">The width (in number of samples) of the map</param>
+        /// <param name="height">The height (in number of samples) of the map</param>
+        /// <param name="step">The interval between samples in the complex plane</param>
+        /// <param name="limit">The maximum of number of iterations to perform</param>
+        /// <param name="output">The resulting iterations map</param>
+        void IterateRange(ComplexDouble start, int width, int height, double step, int limit, int[,] output);
+    }
+}
