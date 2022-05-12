af7413f53923d0c0ce7c61164c35cdad9a769d03
Tor Kristen Haugen <tor.kristen.haugen@novanet.no>
2022-05-12 12:45:11 +0200

    ComplexDouble

---
+++ src/Mandelbrot/ComplexDouble.cs
@@ -0,0 +1,50 @@
+﻿namespace Mandelbrot
+{
+    public struct ComplexDouble : IEquatable<ComplexDouble>
+    {
+        public double A;
+        public double B;
+
+        public ComplexDouble(double a, double b)
+        {
+            A = a;
+            B = b;
+        }
+
+        public bool Equals(ComplexDouble other)
+        {
+            return Equals(A, other.A) && Equals(B, other.B);
+        }
+
+        public override bool Equals(object obj)
+        {
+            return (obj is ComplexDouble cd) ? Equals(cd) : false;
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return (391 + A.GetHashCode()) * 23 + B.GetHashCode();
+            }
+        }
+
+        public override string ToString() => $"({A},{B})";
+
+        public double ModulusSquared => A * A + B * B;
+
+        public double Modulus => Math.Sqrt(ModulusSquared);
+
+        public static ComplexDouble operator +(ComplexDouble x, ComplexDouble y)
+            => new ComplexDouble(x.A + y.A, x.B + y.B);
+
+        public static ComplexDouble operator *(ComplexDouble x, ComplexDouble y)
+            => new ComplexDouble(x.A * y.A - x.B * y.B, x.A * y.B + x.B * y.A);
+
+        public static bool operator ==(ComplexDouble x, ComplexDouble y)
+            => Equals(x, y);
+
+        public static bool operator !=(ComplexDouble x, ComplexDouble y)
+            => !Equals(x, y);
+    }
+}
